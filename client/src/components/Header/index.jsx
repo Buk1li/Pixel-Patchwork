e85d53780cecd70ae7ec38d6937d7dff03ab4f8f
Rylee Jolley <you@example.com>
2024-06-06 21:08:22 -0600

    made adjustments to navbar components

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -6,44 +6,44 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import { Link } from "react-router-dom";
+
 const Header = () => {
   const linkStyle = {
-    textDecoration: "none",
-    color: "#fff",
+    textDecoration: 'none',
+    color: '#fff',
   };
 
   const paperStyle = {
-    padding: "0.5em",
+    padding: '0.5em',
   };
 
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
   return (
     <header className="">
       <Grid
         sx={{
-          backgroundColor: "#2d3e50",
+          backgroundColor: '#2d3e50',
           margin: 0,
-          textAlign: "center",
-          color: "#fff",
+          textAlign: 'center',
+          color: '#fff',
         }}
-        spacing={0}
         container
         direction="row"
         justifyContent="space-around"
         alignItems="center"
-        columns={12}
       >
-        <Grid item xs={4}></Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} sm={4}></Grid>
+        <Grid item xs={12} sm={4}>
           <Link style={linkStyle} to="/">
-            <h1 style={{ fontSize: "60px" }}>r/placeholder</h1>
+            <h1 style={{ fontSize: '60px' }}>r/placeholder</h1>
           </Link>
         </Grid>
 
-        <Grid item xs={4}>
+        <Grid item xs={12} sm={4}>
           {Auth.loggedIn() ? (
             <Button style={linkStyle} onClick={logout}>
               <Paper sx={paperStyle} square={false}>
@@ -51,30 +51,28 @@ const Header = () => {
               </Paper>
             </Button>
           ) : (
-            <>
-              <Grid
-                container
-                direction="row"
-                justifyContent="space-around"
-                alignItems="center"
-                spacing={1}
-              >
-                <Grid item xs={4}>
-                  <Link style={linkStyle} to="/login">
-                    <Paper sx={paperStyle} style={{ fontSize: "20px" }} square={false}>
-                      Login
-                    </Paper>
-                  </Link>
-                </Grid>
-                <Grid item xs={4}>
-                  <Link style={linkStyle} to="/signup">
-                    <Paper sx={paperStyle} square={false} style={{ fontSize: "20px" }}>
-                      Signup
-                    </Paper>
-                  </Link>
-                </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-around"
+              alignItems="center"
+              spacing={1}
+            >
+              <Grid item xs={6} sm={4}>
+                <Link style={linkStyle} to="/login">
+                  <Paper sx={paperStyle} style={{ fontSize: '20px' }} square={false}>
+                    Login
+                  </Paper>
+                </Link>
               </Grid>
-            </>
+              <Grid item xs={6} sm={4}>
+                <Link style={linkStyle} to="/signup">
+                  <Paper sx={paperStyle} square={false} style={{ fontSize: '20px' }}>
+                    Signup
+                  </Paper>
+                </Link>
+              </Grid>
+            </Grid>
           )}
         </Grid>
       </Grid>
